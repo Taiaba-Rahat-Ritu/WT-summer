50b9e47bcba7c5db2cb99878407e2fe567e5d89e
Taiaba-Rahat-Ritu <taiabarahatritu@gmail.com>
2022-06-01 23:28:45 +0600

    completed

---
+++ LabTask1/control/verify.php
@@ -0,0 +1,82 @@
+<?php
+$fname = $_REQUEST["fname"];
+$lname = $_REQUEST["lname"];
+$age = $_REQUEST["age"];
+//$designation = $_POST["designation"];
+//$preferredlanguage = $_POST["preferredlanguage"];
+$email = $_REQUEST["email"];
+$password = $_REQUEST["password"];
+
+if($fname=="")
+{
+    echo "First name is required!" ."<br>";
+}
+else{
+    echo "First name is" .$fname;
+}
+if($lname=="")
+{
+    echo "Last name is required!" ."<br>";
+}
+else{
+    echo "Last name is" .$lname;
+}
+if($email=="")
+{
+    echo "Email is required!" ."<br>";
+}
+else{
+    echo "Email is" .$email;
+}
+
+if(strlen($password) <8)
+{
+    echo "Password should be more than 8 characters" ."<br>";
+}
+else
+{
+    echo "Password is valid" ."<br>";
+}
+
+
+
+if(isset($_REQUEST["designation"]))
+{
+    $designation = $_REQUEST["designation"];
+    echo "Your designation is " . $designation."<br>";
+    
+}
+else
+{
+    echo "Must select designation" . "<br>";
+}
+
+
+if(isset($_REQUEST["JAVA"]) || isset($_REQUEST["PHP"]) || isset($_REQUEST["C++"])){
+    if(isset($_REQUEST["JAVA"]) && isset($_REQUEST["PHP"])){
+        echo "Preferred language is " . $_REQUEST["JAVA"] . " and " . $_REQUEST["PHP"];
+    }
+    elseif(isset($_REQUEST["PHP"]) && isset($_REQUEST["C++"])){
+        echo "Preferred language is " . $_REQUEST["PHP"] . " and " . $_REQUEST["C++"];
+    }
+    elseif(isset($_REQUEST["JAVA"]) && isset($_REQUEST["C++"])){
+        echo "Preferred language is " . $_REQUEST["JAVA"] . " and " . $_REQUEST["C++"];
+    }
+    elseif(isset($_REQUEST["JAVA"])){
+        echo "Preferred language is " . $_REQUEST["JAVA"];
+    }
+    elseif(isset($_REQUEST["PHP"])){
+        echo "Preferred language is " . $_REQUEST["PHP"];
+    }
+    elseif(isset($_REQUEST["C++"])){
+        echo "Preferred language is " . $_REQUEST["C++"];
+    }
+
+}
+else
+{
+    echo "Must select preferred language" . "<br>";
+}
+
+
+?>
